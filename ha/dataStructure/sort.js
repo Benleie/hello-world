b7b02269bc2709522b7ee8d40ab3e5f9dd0bc057
Ben Lee <1019229581@qq.com>
2017-10-01 21:31:39 +0800

    bubble sort

--- ha/dataStructure/sort.js
+++ ha/dataStructure/sort.js
@@ -10,12 +10,6 @@ function showTime() {
 }
 showTime()
 
-
-var arrQuick =  [2,44,54,636,1,64,63,73,3,44,38,5,47,3434,15,36,26,27,2,46,4,19,50,48],
-	arrSelect = [2,44,54,636,1,64,63,73,3,44,38,5,47,3434,15,36,26,27,2,46,4,19,50,48],
-	arrInsert = [2,44,54,636,1,64,63,73,3,44,38,5,47,3434,15,36,26,27,2,46,4,19,50,48],
-	arroo
-
 let quickSort = function(arr) {
 	if(arr.length <= 1) return arr;
 	let pivotIndex = Math.floor(arr.length/2);
@@ -32,10 +26,7 @@ let quickSort = function(arr) {
 	return quickSort(left).concat([pivot],quickSort(right));
 }
 
-// console.time('quickSort')
-// // cl(quickSort(arr1))
-// console.timeEnd('quickSort')
-
+//每次选出最小的那个元素,和当前元素交换位置。
 function selectSort(arr){
 	let minIndex,temp;
 	console.time('selectSort')
@@ -51,9 +42,7 @@ function selectSort(arr){
 	console.timeEnd('selectSort');
 	return arr;
 }
-// selectSort(arr1)
-// cl(selectSort(arr1))
-
+//插入排序
 function insertSort(arr){
 	if({}.toString.call(arr).slice(8,-1) === 'Array'){
 		console.time('insertSort')
@@ -72,6 +61,65 @@ function insertSort(arr){
 		return 'argument is not a array!'
 	}
 }
-insertSort(arrInsert)
-// cl(insertSort(arr2))
+//先二分查找到位置，再插入
+function binaryInsertSort(arr){
+	if({}.toString.call(arr).slice(8,-1) === "Array"){
+		console.time('binaryInsertSort')
+		for(let i = 1; i<arr.length; i++){
+			let value = arr[i],
+				left = 0,
+				right = i - 1;
+			while(left<=right){
+				let middle = parseInt((left+right)/2)
+				if(value < arr[middle])
+					right = middle - 1;
+				else
+					left = middle + 1
+			}
+			for(let j = i; j > left; j--)
+				arr[j] = arr[j-1]
+			arr[left] = value
+		}
+		console.timeEnd('binaryInsertSort')
+		return arr;
+	} else 
+		return 'argument is not a array!'
+}
+//每次从头开始，把最大的元素冒到后面
+function bubbleSort(arr){
+	console.time('bubbleSort')
+		for(let j = 1; j<arr.length - i; j++){
+			if(arr[j-1] > arr[j]){
+				let temp = arr[j];
+				arr[j] = arr[j-1]
+				arr[j-1] = temp;
+			}
+		for(let i = 0; i<arr.length; i++){
+	}
+	}
+	console.timeEnd('bubbleSort')
+	return arr;
+}
+//改进冒泡排序，每次冒完泡，记住最后那个冒泡的位置，下次冒泡就只需要冒到标记的位置。
+function bubbleSort2(arr){
+	console.time('bubbleSort2')
+	let i = arr.length;
+	while(i > 0){
+		let pos = 0;
+		for(let j = 1; j < i; j++){
+			if(arr[j-1] > arr[j]){
+				pos = j 
+				let temp = arr[j];
+				arr[j] = arr[j-1]
+				arr[j-1] = temp;
+			}
+		}
+		i = pos;
+	}
+	console.timeEnd('bubbleSort2')
+	return arr;
+}
+
+
+
 
