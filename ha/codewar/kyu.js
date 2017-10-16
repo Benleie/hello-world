a249723a387d018019d957120215e0b97ffbf162
Ben Lee <1019229581@qq.com>
2017-10-16 21:43:13 +0800

    kyu 6

--- ha/codewar/kyu.js
+++ ha/codewar/kyu.js
@@ -15,7 +15,35 @@ showTime();
 
 
 
-
+function deleteNth(arr,x){
+	let uniqueArr = [],
+		countArr = [],
+		resultArr = []
+    for(let i = 0;i<arr.length; i++){
+        if(!uniqueArr.includes(arr[i]))
+            uniqueArr.push(arr[i])
+    }
+    for(let i = 0; i<uniqueArr.length; i++)
+    	countArr[i] = 0;
+    for(let i = 0;i<arr.length; i++){
+    	for(let j = 0;j<uniqueArr.length; j++){
+    		if(uniqueArr[j] === arr[i] && countArr[j] < x){
+    			resultArr.push(arr[i])
+    			countArr[j]++;
+    		}
+    	}
+    }
+	return resultArr;
+}
+function deleteNth(arr,x) {
+  var cache = {};
+  return arr.filter(function(n) {
+    cache[n] = (cache[n]||0) + 1;
+    return cache[n] <= x;
+  });
+}
+console.log(deleteNth([1,1,3,3,7,2,2,2,2], 3))
+console.log(deleteNth([1,2,1,3,3,7,2,2,2], 3))
 
 
 function findOdd(arr){
--- read/Learn JS dataStructure/5linkedList.js
+++ read/Learn JS dataStructure/5linkedList.js
@@ -19,7 +19,26 @@ function LinkedList(){
 		length++
 	}
 	this.insert = function(position, element){
-
+		if(position>=0 && position<=length){
+			let node = new Node(element),
+				current = head,
+				previous,
+				index = 0;
+			if(position === 0){
+				node.next = current;
+				head = node;
+			} else {
+				while(index++ < position){
+					previous = current;
+					current = current.next;
+				}
+				node.next = current;
+				previous.next = node;
+			}
+			length++;
+			return true;
+		} else
+			return false;
 	}
 	this.removeAt = function(position){
 		if(position > -1 && position < length){
@@ -28,15 +47,51 @@ function LinkedList(){
 				index = 0;
 			if(position === 0)
 				head = current.next;
-
-		}
+			else {
+				while(index++<position){
+					previous = current;
+					current = current.next;
+				}
+				previous.next = current.next;
+			}
+			length--;
+			return current.element;
+		} else
+			return null;
 	}
-	this.remove = function(element){}
-	this.indexOf = function(element){}
-	this.isEmpty = function(){}
-	this.size = function(){}
-	this.toString = function(){}
-	this.print = function(){}
+	this.remove = function(element){
+		let index = this.indexOf(element);
+		return this.removeAt(index);
+	}
+	this.indexOf = function(element){
+		let current = head,
+			index = -1;
+		while(current){
+			if(element === current.element)
+				return index;
+			index++;
+			current = current.next;
+		}
+		return -1;
+	}
+	this.isEmpty = function(){
+		return length === 0;
+	}
+	this.size = function(){
+		return length;
+	}
+	this.toString = function(){
+		let current = head,
+			string = '';
+		while(current) {
+			string = current.element;   //why not +
+			current = current.next
+		}
+		return string;
+	}
+	this.getHead = function(){
+		return head;
+	}
 }
 
 var list = new LinkedList();
