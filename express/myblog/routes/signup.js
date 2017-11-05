b2a36fc0f20ccf67e211e8157e5ff68e8578a6bb
Ben Lee <1019229581@qq.com>
2017-11-05 17:48:09 +0800

    signin

--- express/myblog/routes/signup.js
+++ express/myblog/routes/signup.js
@@ -21,47 +21,47 @@ router.post('/', checkNotLogin, function(req, res, next) {
   var password = req.fields.password;
   var repassword = req.fields.repassword;
 
-// 校验参数
-try {
-if (!(name.length >= 1 && name.length <= 10)) {
-  throw new Error('名字请限制在 1-10 个字符');
-}
-if (['m', 'f', 'x'].indexOf(gender) === -1) {
-  throw new Error('性别只能是 m、f 或 x');
-}
-if (!(bio.length >= 1 && bio.length <= 30)) {
-  throw new Error('个人简介请限制在 1-30 个字符');
-}
-if (!req.files.avatar.name) {
-  throw new Error('缺少头像');
-}
-if (password.length < 6) {
-  throw new Error('密码至少 6 个字符');
-}
-if (password !== repassword) {
-  throw new Error('两次输入密码不一致');
-}
-} catch (e) {
-// 注册失败，异步删除上传的头像
-fs.unlink(req.files.avatar.path);
-req.flash('error', e.message);
-return res.redirect('/signup');
-}
+  // 校验参数
+  try {
+    if (!(name.length >= 1 && name.length <= 10)) {
+      throw new Error('名字请限制在 1-10 个字符');
+    }
+    if (['m', 'f', 'x'].indexOf(gender) === -1) {
+      throw new Error('性别只能是 m、f 或 x');
+    }
+    if (!(bio.length >= 1 && bio.length <= 30)) {
+      throw new Error('个人简介请限制在 1-30 个字符');
+    }
+    if (!req.files.avatar.name) {
+      throw new Error('缺少头像');
+    }
+    if (password.length < 6) {
+      throw new Error('密码至少 6 个字符');
+    }
+    if (password !== repassword) {
+      throw new Error('两次输入密码不一致');
+    }
+  } catch (e) {
+    // 注册失败，异步删除上传的头像
+    fs.unlink(req.files.avatar.path);
+    req.flash('error', e.message);
+    return res.redirect('/signup');
+  }
 
-// 明文密码加密
-password = sha1(password);
+  // 明文密码加密
+  password = sha1(password);
 
-// 待写入数据库的用户信息
-var user = {
-	name: name,
-	password: password,
-	gender: gender,
-	bio: bio,
-	avatar: avatar
-};
+  // 待写入数据库的用户信息
+  var user = {
+  	name: name,
+  	password: password,
+  	gender: gender,
+  	bio: bio,
+  	avatar: avatar
+  };
 
 
-// 用户信息写入数据库
+  // 用户信息写入数据库
   UserModel.create(user)
     .then(function (result) {
       // 此 user 是插入 mongodb 后的值，包含 _id
