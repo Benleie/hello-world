07f0c8d6e1542345ed450d149833f35d69dc92b4
Ben Lee <1019229581@qq.com>
2017-12-09 23:04:34 +0800

    Z()

---
+++ study/zepto/2.js
@@ -0,0 +1,24 @@
+// console.log(Zepto)
+console.log($("span",'p'))
+
+
+
+/*
+ar fn = function(){return 1}
+var ff = fn;
+ff = function(){return 2}
+console.log(fn)
+
+
+
+var a = "a"
+	,aa = "aa"
+	,b
+b = a + "s"
+console.log(b === "a" && (b = aa))
+console.log(b)
+*/
+
+v
+
+
