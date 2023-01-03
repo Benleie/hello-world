c7261be366db4a6d8a7728f2846c160f79e11869
李奔 <liben@leyopharm.com>
2023-01-03 18:11:33 +0800

    demo: await-in-loop

---
+++ try/promise/await-in-loop.js
@@ -0,0 +1,27 @@
+
+
+// const p1 = new Promise((resolve, reject) => {
+//   console.log('p1')
+//   resolve('p1 resolved')
+// })
+// const p2 = new Promise((resolve, reject) => {
+//   console.log('p2')
+//   resolve('p2 resolved')
+// })
+
+const randomReturn = () => {
+  return new Promise((resolve, reject) => {
+    // 1, 1.5, 2, ..., 4.5, 5
+    const time = Math.floor(Math.random(0, 1)*8) / 2 + 1
+    setTimeout(() => {
+      resolve(time)
+    }, time*1000)
+  })
+}
+const res = [];
+[1, 2, 3].forEach(async() => {
+  const number = await randomReturn()
+  res.push(number)
+  console.log(res)
+})
+console.log(res)
