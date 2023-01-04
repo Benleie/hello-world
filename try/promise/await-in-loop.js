0412cab32bad76c7fd24320f2f74260d0244e381
李奔 <liben@leyopharm.com>
2023-01-04 12:57:55 +0800

    feat(promise): for-loop

--- try/promise/await-in-loop.js
+++ try/promise/await-in-loop.js
@@ -1,16 +1,7 @@
 
-
-// const p1 = new Promise((resolve, reject) => {
-//   console.log('p1')
-//   resolve('p1 resolved')
-// })
-// const p2 = new Promise((resolve, reject) => {
-//   console.log('p2')
-//   resolve('p2 resolved')
-// })
-
 const randomReturn = (item) => {
   console.log(item)
+  console.time(item)
   return new Promise((resolve, reject) => {
     // 1, 1.5, 2, ..., 4.5, 5
     const time = Math.floor(Math.random(0, 1)*8) / 2 + 1
@@ -19,10 +10,23 @@ const randomReturn = (item) => {
     }, time*1000)
   })
 }
-const res = [];
-[1, 2, 3].forEach(async(item) => {
-  const number = await randomReturn(item)
-  res.push(number)
-  console.log(res)
-})
-console.log(res)
+const testForEach = () => {
+  const res = [];
+  [1, 2, 3].forEach(async(item) => {
+    const number = await randomReturn(item)
+    res.push(number)
+    console.log(res)
+    console.timeEnd(item)
+  })
+}
+// testForEach()
+
+const testForLoop = async() => {
+  const res = [];
+  for (let index = 0; index < 3; index++) {
+    const number = await randomReturn(index)
+    res.push(number)
+    console.timeEnd(index)
+  }
+}
+testForLoop()
