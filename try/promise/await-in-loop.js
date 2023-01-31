1574dbf75bb1224e886776ac3ec7e2714eb7affb
李奔 <liben@leyopharm.com>
2023-01-31 18:47:12 +0800

    feat: 整理promise

--- try/promise/await-in-loop.js
+++ try/promise/await-in-loop.js
@@ -1,6 +1,9 @@
 
+// 想要的效果：
+// 遍历请求，一个接口返回结果之后再请求下一个；把返回的结果按照请求的顺序依次存入数组
+
 const randomReturn = (item) => {
-  console.log(item)
+  console.log('start async task:', item)
   console.time(item)
   return new Promise((resolve, reject) => {
     // 1, 1.5, 2, ..., 4.5, 5
@@ -10,6 +13,20 @@ const randomReturn = (item) => {
     }, time*1000)
   })
 }
+
+const testCallbackThen = () => {
+  const res = [];
+  [1, 2, 3].forEach((item) => {
+    randomReturn(item).then((number) => {
+      res.push(number)
+      console.log(res)
+      console.timeEnd(item)
+    })
+  })
+  console.log(res)
+}
+// testCallbackThen()
+
 const testForEach = () => {
   const res = [];
   [1, 2, 3].forEach(async(item) => {
@@ -22,6 +39,7 @@ const testForEach = () => {
 }
 // testForEach()
 
+// 
 const testForLoop = async() => {
   const res = [];
   for (let index = 0; index < 3; index++) {
